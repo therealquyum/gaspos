2d47614d08932d8335c42e58b1efc2e24b242ef5
Abdul Quyum <abdulquyum246@gmail.com>
2026-02-22 21:49:53 +0100

    solving offline chache

--- sw.js
+++ sw.js
@@ -1,25 +1,22 @@
-const CACHE_NAME = "gas-pos-cache-v1";
+const CACHE_NAME = "gas-pos-cache-v2";
+
 const urlsToCache = [
   "./",
   "./index.html",
   "./script.js",
-  "./manifest.json",
-  "./style.css"
+  "./manifest.json"
 ];
 
-// Install service worker and cache files
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Activate service worker
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-// Fetch cached files
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
